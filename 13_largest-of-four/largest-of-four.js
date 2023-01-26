2be6d7973a05974ef723e2c570a66ebf6ccf7bae
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-26 22:26:52 +0100

    Update largest-of-four.js

--- 13_largest-of-four/largest-of-four.js
+++ 13_largest-of-four/largest-of-four.js
@@ -7,3 +7,16 @@ export const largestOfFour = (arrays) => {
 
   return resultArray;
 };
+
+
+export const largestOfFour = (arrays) => {
+  return arrays.map((subArray) => Math.max(...subArray));
+};
+
+// This code uses the Math.max() function, along with the spread operator (...), to find the maximum value in each sub-array. This eliminates the need to use the reduce method and manually compare values within each sub-array. The spread operator allows to pass an array of elements to Math.max() as individual arguments.
+
+export const largestOfFour = (arrays) => {
+  return arrays.map((subArray) => Math.max.apply(null, subArray));
+};
+
+// This code uses the Math.max.apply(null, subArray) to find the maximum value in each sub-array. This eliminates the need to use the reduce method and manually compare values within each sub-array.

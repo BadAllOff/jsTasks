c3bd39b0720a052330d5ffb8314b272c2703f80d
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-26 22:31:00 +0100

    Update clone-arr.js

--- 14_clone-arr/clone-arr.js
+++ 14_clone-arr/clone-arr.js
@@ -1,3 +1,14 @@
 export const cloneArr = (array) => {
   return array.map((element) => element);
 };
+
+export const cloneArray = (arr) => [...arr];
+export const cloneArray = (arr) => arr.slice();
+export const cloneArray = (arr) => Array.from(arr);
+export const cloneArray = (arr) => JSON.parse(JSON.stringify(arr));
+
+// This creates a deep copy of the original array by first converting the original array to a JSON string using JSON.stringify(), and then parsing the JSON string back into a new array using JSON.parse().
+
+// Please note that the above mentioned method 4 will work fine for simple arrays, but it fails to clone complex objects and functions.
+
+// It is also worth noting that these methods will only create a shallow copy of the array, which means that if the original array contains objects or other arrays, the new array will contain references to the same objects and arrays as the original. If you need to create a deep copy of the array, you will need to use a more complex method, such as using a recursive function or a library like Lodash.

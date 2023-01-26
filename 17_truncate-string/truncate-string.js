993fd2c344c9f4f8f010139def421fdb3db62b2b
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-26 22:37:47 +0100

    Update truncate-string.js

--- 17_truncate-string/truncate-string.js
+++ 17_truncate-string/truncate-string.js
@@ -4,3 +4,19 @@ export const truncateString = (string, maxLength) => {
 
   return string.slice(0, stringLength) + '...';
 };
+
+
+export const truncateString = (string, maxLength) => {
+  if (string.length <= maxLength) return string;
+  return string.slice(0, maxLength > 3 ? maxLength - 3 : maxLength) + '...';
+}
+
+export const truncateString = (string, maxLength) => {
+    if (string.length <= maxLength) return string;
+    return string.substr(0, maxLength > 3 ? maxLength - 3 : maxLength) + '...';
+}
+
+export const truncateString = (string, maxLength) => {
+    if (typeof maxLength !== 'number' || string.length <= maxLength) return string;
+    return string.substr(0, maxLength > 3 ? maxLength - 3 : maxLength) + '...';
+}

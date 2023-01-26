94457ae6bb55a403376c767fd4303fb476afc63e
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-26 22:52:53 +0100

    Update truncate-array.js

--- 20_truncate-array/truncate-array.js
+++ 20_truncate-array/truncate-array.js
@@ -1,3 +1,14 @@
 export const truncateArray = (array, number) => {
   return array.slice(number, array.length);
 };
+
+export const truncateArray = (array, number) => {
+  return array.slice(number);
+};
+// This code eliminates the need for passing the second parameter array.length as the second argument for the slice() method, because when the second parameter is not provided, the slice method assumes that the end of the array is the second argument.
+
+export const truncateArray = (array, number) => {
+  return array.splice(number);
+};
+
+// This code eliminates the need for passing the second parameter array.length as the second argument for the splice() method, because when the second parameter is not provided, the splice method assumes that the end of the array is the second argument.

343f0534c1fd872af876b32a6f3e4f9525425818
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-29 12:00:07 +0100

    Update people-in-bus.js

--- 30_people-in-bus/people-in-bus.js
+++ 30_people-in-bus/people-in-bus.js
@@ -9,3 +9,10 @@ export const peopleInBus = (array) => {
 
   return getIn - getOut;
 };
+
+
+export const peopleInBus = (array) => {
+  return array.reduce((sum, [getIn, getOut]) => sum + getIn - getOut, 0);
+};
+
+// useing the reduce method to accumulate the values of the number of people getting in and the number of people getting out of the bus, and returns the difference between the two values as the final result, which should be the number of people still in the bus.

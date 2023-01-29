99604a832d4eb8e4bad825e4be8619db895bf9b2
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-29 11:52:44 +0100

    Update average-mark.js

--- 26_average-mark/average-mark.js
+++ 26_average-mark/average-mark.js
@@ -1,3 +1,7 @@
 export const averageMark = (array) => {
   return Math.floor(array.reduce((sum, element) => sum + element, 0) / array.length);
 };
+
+export const averageMark = (array) => {
+  return array.reduce((sum, element) => sum + element, 0) / array.length;
+};

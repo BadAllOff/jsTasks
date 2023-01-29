e88340ccdcc01a45530434c03f44b717361b82e8
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-29 11:36:01 +0100

    Update remove-falsy-values.js

--- 22_remove-falsy-values/remove-falsy-values.js
+++ 22_remove-falsy-values/remove-falsy-values.js
@@ -3,3 +3,10 @@ export const removeFalsyValues = (array) => {
     return elem;
   });
 };
+
+
+export const removeFalsyValues = (array) => {
+  return array.filter(Boolean);
+};
+
+// This optimized version uses Boolean as the callback function for filter instead of a custom anonymous function. The Boolean function returns true for values that are truthy, and false for values that are falsy. In this case, the filter method will only keep the truthy values in the array, effectively removing the falsy values.

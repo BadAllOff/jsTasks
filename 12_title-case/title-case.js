4e60c4bcf2f265be0840766be61a24ad8ba8b05b
sbadalov-grid <114946452+sbadalov-grid@users.noreply.github.com>
2023-01-29 10:41:49 +0100

    Add: jest babel etc

--- 12_title-case/title-case.js
+++ 12_title-case/title-case.js
@@ -1,16 +1,15 @@
-export const titleCase = (string) => {
-  const wordsList = string.toLowerCase().split(' ');
-  return wordsList
-    .map((word) => {
-      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    })
-    .join(' ');
-};
+// export const titleCase = (string) => {
+//   const wordsList = string.toLowerCase().split(' ');
+//   return wordsList
+//     .map((word) => {
+//       return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+//     })
+//     .join(' ');
+// };
 
-
-export const titleCase = (string) => {
-  return string.replace(/(^|\s)[a-z]/g, (match) => match.toUpperCase());
-};
+// export const titleCase = (string) => {
+//   return string.replace(/(^|\s)[a-z]/g, (match) => match.toUpperCase());
+// };
 
 export const titleCase = (string) => {
   return string.replace(/\b[a-z]/gi, (char) => char.toUpperCase());

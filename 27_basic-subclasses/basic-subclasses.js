5d2abf7d899131e321b07a7a121d90dc9d7150a2
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-29 11:54:19 +0100

    Update basic-subclasses.js

--- 27_basic-subclasses/basic-subclasses.js
+++ 27_basic-subclasses/basic-subclasses.js
@@ -24,3 +24,33 @@ class God {
 }
 
 export { God, Human, Man, Woman };
+
+
+class Human {
+  constructor(gender, name) {
+    this.sex = gender;
+    this.name = name;
+  }
+}
+
+class Man extends Human {
+  sex = 'male';
+  constructor(name) {
+    super(this.sex, name);
+  }
+}
+
+class Woman extends Human {
+  sex = 'female';
+  constructor(name) {
+    super(this.sex, name);
+  }
+}
+
+class God {
+  static create() {
+    return [new Man('Adam'), new Woman('Eve')];
+  }
+}
+
+export { God, Human, Man, Woman };

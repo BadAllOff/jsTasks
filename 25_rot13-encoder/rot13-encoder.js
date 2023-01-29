6b8899f05b81bf010472d3919c5fa2409706b037
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-29 11:47:19 +0100

    Update rot13-encoder.js

--- 25_rot13-encoder/rot13-encoder.js
+++ 25_rot13-encoder/rot13-encoder.js
@@ -13,3 +13,27 @@ export const rot13Encoder = (string) => {
 
   return result.join('');
 };
+
+
+
+export const rot13Encoder = (string) => {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const charMap = new Map();
+  for (let i = 0; i < 26; i++) {
+    charMap.set(alphabet[i], alphabet[(i + 13) % 26]);
+  }
+
+  let result = '';
+  for (const char of string) {
+    if (alphabet.includes(char)) {
+      result += charMap.get(char);
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
+};
+
+
+// This optimized version uses a Map to store the mapping between the original characters and their rot13 encoded counterparts. This allows us to encode characters by looking up their encoded counterpart in the map, which is much faster than searching for the character in the alphabet and computing its encoded counterpart every time. Additionally, the original string is processed character by character instead of using split and map to improve performance.

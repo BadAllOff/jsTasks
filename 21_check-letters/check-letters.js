54fe9a3052db4f66e821d5e408b87b5747d20762
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-29 11:34:38 +0100

    Update check-letters.js

--- 21_check-letters/check-letters.js
+++ 21_check-letters/check-letters.js
@@ -14,3 +14,19 @@ export const checkLetters = (array) => {
 
   return searchString === result;
 };
+
+
+export const checkLetters = (array) => {
+  let string = array[0].toLowerCase();
+  let searchString = array[1].toLowerCase();
+
+  for (let char of searchString) {
+    if (string.indexOf(char) === -1) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// This optimized version avoids creating a new array with map and concatenating the result with join, which is not necessary for this particular task. Instead, it uses a for loop to iterate over the searchString and check if each character is present in the string. If a character is not present, the function returns false immediately, avoiding unnecessary looping and string concatenation.

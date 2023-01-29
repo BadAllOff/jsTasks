f7b2efaab4d4a40df48f7d4a040e6e72646de4f9
BadAllOff <BadAllOff@users.noreply.github.com>
2023-01-29 11:43:20 +0100

    Update index-to-insert.js

--- 24_index-to-insert/index-to-insert.js
+++ 24_index-to-insert/index-to-insert.js
@@ -8,3 +8,27 @@ export const indexToInsert = (array, insertIndex) => {
     if (index + 1 == array.length) return index + 1;
   }
 };
+
+
+export const indexToInsert = (array, insertIndex) => {
+  const sortedArr = array.slice().sort((a, b) => a - b);
+
+  let left = 0;
+  let right = array.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (sortedArr[mid] === insertIndex) {
+      return mid;
+    } else if (sortedArr[mid] < insertIndex) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return left;
+};
+
+
+// This optimized version uses binary search to find the index at which insertIndex should be inserted into the sorted array. By using binary search, we can improve the performance of the function for large arrays.
+
+// Additionally, the sort method has been updated to use a subtract operation as the compare function to sort the array in ascending order, which is more efficient than using the ternary operator a < b ? -1 : 1. The original array has been cloned using the slice method instead of the map method, which is more efficient for cloning arrays.
